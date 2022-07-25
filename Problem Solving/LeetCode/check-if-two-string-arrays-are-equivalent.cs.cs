2518a08ae92fc2b8181b36087a6608bf55da3063
Hossam Hamdy <hossam192216@fci.bu.edu.eg>
2022-07-25 14:36:24 +0200

    🎉 lets try C# :)

---
+++ Problem Solving/LeetCode/check-if-two-string-arrays-are-equivalent.cs.cs
@@ -0,0 +1,20 @@
+﻿// https://leetcode.com/problems/check-if-two-string-arrays-are-equivalent/
+
+bool ArrayStringsAreEqual(string[] word1, string[] word2) {
+
+    StringBuilder text1 = new StringBuilder("");
+    StringBuilder text2 = new StringBuilder("");
+
+    foreach(string w1 in word1){
+        text1.Append(w1);
+    }
+    foreach(string w2 in word2){
+        text2.Append(w2);
+    }
+
+    if (text1.Equals(text2)){
+        return true;
+    }else{
+        return false;
+    }
+}
